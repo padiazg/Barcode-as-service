4dc185db930b9bb5e8af2b9499051f71e287399b
Patricio Díaz <padiazg@gmail.com>
2016-05-09 14:46:30 -0400

    Solved a problem with escale and fonts generating EAN13 Added generation for DataMatrix code

--- barcode-service.js
+++ barcode-service.js
@@ -15,7 +15,6 @@ var routes         = require('./routes/routes.js');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 
 app.use('/', routes);
 
@@ -23,7 +22,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride());
-app.use(express.static(path.join(__dirname, 'static')));
 
 var server = app.listen(process.env.PORT || 3000, function() {
    console.log('Barcode server listening on port %d', server.address().port);
